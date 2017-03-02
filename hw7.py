05a8e6b83a8169d066f173c692e93dd256f2b2b7
jcyng <jessicang@Jessicas-MacBook-Air.local>
2017-03-01 21:14:45 -0800

    uploaded hw7.py

--- hw7.py
+++ hw7.py
@@ -1,17 +1,26 @@
-from sortedcontainers import SortedDict
+"""
+This script takes entries of users and corresponding usernames in a "database", and allows the user to add, remove, and lookup entries from this "database".
+The user must reference the menu options (i.e. 1 - 5) presented to determine the action he/she wants to take. 
+If the user enters an integer that is not between 1 - 5, the script will continue asking the user to input a number.
+If the user enters a non-integer, for example a float or a string, a value error would be captured and the menu would be displayed again.
+Note that capitalization matters when looking up users and corresponding usernames.
 
-#
-# use this to show options
-#
+"""
+try: 
+    from sortedcontainers import SortedDict
+except ImportError:
+    print("Import Error")
+
+# Show menu options
 def print_menu():
     print('1. Print Users')
     print('2. Add a User')
     print('3. Remove a User')
-    print('4. Lookup a Phone Number')
+    print('4. Lookup Username')
     print('5. Quit')
     print()
 
-# Create dictionary with key = Names, value = user_name
+# Dictionary with key = Names, value = user_name
 usernames = SortedDict()
 usernames['Summer'] = 'summerela'
 usernames['William'] = 'GoofyFish'
@@ -19,46 +28,63 @@ usernames['Steven'] = 'LoLCat'
 usernames['Zara'] = 'zanyZara'
 usernames['Renato'] = 'songDude'
 
-# setup counter to store menu choice
+# Counter for menu choice
 menu_choice = 0
 
-#display your menu
+#display menu
 print_menu()
 
-# as long as the menu choice isn't "quit" get user options
-while menu_choice != 5:
+# User input options results
+
+try:
+    while menu_choice != 5:
     # get menu choice from user
-    menu_choice = int(input("Type in a number (1-5): "))
-    
+        menu_choice = int(input("Type in a number (1-5): "))
+except ValueError:
+    print("Value Error: Re-run script and type in an integer between 1 and 5")
+
+
     # view current entries
     if menu_choice == 1:
-        print("Current Users:")
-        for x,y in usernames.items():
-            print("Name: {} \tUser Name: {} \n".format(x,y))
-            
+        try:
+            print("Current Users:")
+            for x,y in usernames.items():
+                print("Name: {} \tUser Name: {} \n".format(x,y))
+        except KeyError:
+            print("Key Error") 
+
     # add an entry
     elif menu_choice == 2:
-        print("Add User")
-        name = input("Name: ")
-        username = input("User Name: ")
-        usernames[name] = username
-        
+        try:
+            print("Add User")
+            name = input("Name: ")
+            username = input("User Name: ")
+            usernames[name] = username
+        except KeyError:
+            print("Key Error")
+       
     # remove an entry
     elif menu_choice == 3:
-        print("Remove User")
-        name = input("Name: ")
-        if name in usernames:
-            pass # delete that entry
+        try:
+            print("Remove User")
+            name = input("Name: ")
+            if name in usernames:
+                del usernames[name]
+        except KeyError:
+            print("Key Error")
 
-    # view user name      
+    # look up user name from user      
     elif menu_choice == 4:
-        print("Lookup User")
-        name = input("Name: ")
-        if name in usernames:
-            pass # print the username
-        else:
-            pass # print username not found
-    
-    # is user enters something strange, show them the menu
+        try:
+            print("Lookup User")
+            name = input("Name: ")
+            if name in usernames:
+               print("User Name: {}".format(usernames[name]))
+            else:
+              print("username not found")
+        except KeyError:
+            print("Key Error")
+
+    # if user enters something strange, show them the menu
     elif menu_choice != 5:
         print_menu()
